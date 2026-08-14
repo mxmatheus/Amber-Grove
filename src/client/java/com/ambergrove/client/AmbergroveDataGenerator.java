d762da8fac2a3c51bc91b532eb5a3cf149bd71da
mxmatheus <batuhan.koksal699@gmail.com>
2026-08-14 13:33:36 +0300

    Initial commit: Minecraft mod project structure

---
+++ src/client/java/com/ambergrove/client/AmbergroveDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ambergrove.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AmbergroveDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
